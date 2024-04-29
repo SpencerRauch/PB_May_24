62ed7d9e1b7724cb363619b89d23aeeaa4a395e1
Spencer Rauch <rauchs@gmail.com>
2024-04-29 13:34:11 -0700

    day 1 afternoon lecture

---
+++ Day1_VariablesDataTypes.js
@@ -0,0 +1,53 @@
+// this is a comment!
+/*             
+a
+multi
+line
+comment
+*/
+var myVar;
+var whateverIWant = "Value";
+
+// let, const
+
+console.log(myVar);
+
+//data types
+
+//strings
+// a collection of characters 
+
+var myString = "This is a string";
+var anotherString = 'This is also a string';
+var myQuote = ' Bob said, "This is a quote." ';
+var contraction = " This won't be a problem ";
+var stringInterpolation = `This is for putting out variables ${whateverIWant}`;
+console.log(stringInterpolation);
+
+var myconcat = myQuote + anotherString;
+
+
+//number
+var number = 7;
+//64bit floating point
+var float = 7.01;
+var stringInterpolation2 = `This is for putting out variables ${number}`;
+console.log(stringInterpolation2);
+
+var sum = number + float;
+var product = number * float;
+
+console.log(sum + myString);
+
+//boolean
+//true or false -- will be used in conditionals!
+var loggedIn = false;
+var lovesDogs = true;
+
+
+
+//null / undefined
+var emptyBox = null; //has the meaning of NOTHING EMPTY the LACK OF CONTENT
+var newVar = undefined; //has the meaning of UNKNOWN, NOT YET DEFINED
+
+
