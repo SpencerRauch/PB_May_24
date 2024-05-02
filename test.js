e318a9564365c42e53bddf8a2932588130342156
Spencer Rauch <rauchs@gmail.com>
2024-05-02 12:53:36 -0700

    some test code explaining nested for loops

--- test.js
+++ test.js
@@ -2,3 +2,16 @@
 // TODO: This is something I need to finish later
 // * Info
 // colors
+
+
+//nested loops
+
+for (var x = 0; x < 5; x++){ //outerloop runs 5 times
+    console.log("I'm running the outer, x is",x )
+    for (var o = 0; o < 5; o++){ //innerloop runs 5 times
+        console.log("I'm running the inner, o is",o )
+    }
+    console.log("\n")
+}
+
+//5 x 5 = 25 iterations for inner loop
