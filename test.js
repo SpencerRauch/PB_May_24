c2fff8893374b9cf3366b8cdf1b9a1c974acdbf1
Spencer Rauch <rauchs@gmail.com>
2024-05-02 12:54:27 -0700

    notes

--- test.js
+++ test.js
@@ -8,7 +8,7 @@
 
 for (var x = 0; x < 5; x++){ //outerloop runs 5 times
     console.log("I'm running the outer, x is",x )
-    for (var o = 0; o < 5; o++){ //innerloop runs 5 times
+    for (var o = 0; o < 5; o++){ //innerloop runs 5 times FOR EVERY OUTER LOOP
         console.log("I'm running the inner, o is",o )
     }
     console.log("\n")
